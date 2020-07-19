c320b460fe215165197c23b9a2082cbd05170840
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2020-07-19 18:15:18 -0300

    Revert "test: ensure AxiosHttpClient calls axios with correct body"
    This reverts commit 25224a3e1a7a9238884782cf6e4a7a7b6e82b4e4.

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -2,18 +2,10 @@
 /* eslint-disable class-methods-use-this */
 
 import axios from 'axios'
-import {
-  HttpPostParams,
-  HttpResponse,
-  HttpPostClient,
-} from '../../../data/protocols/http'
+import { HttpPostParams } from '../../../data/protocols/http'
 
-export class AxiosHttpClient implements HttpPostClient<any, any> {
-  async post(params: HttpPostParams<any>): Promise<HttpResponse<any>> {
-    const httpResponse = await axios.post(params.url, params.body)
-    return {
-      statusCode: httpResponse.status,
-      body: httpResponse.data,
-    }
+export class AxiosHttpClient {
+  async post(params: HttpPostParams<any>): Promise<void> {
+    await axios.post(params.url, params.body)
   }
 }
