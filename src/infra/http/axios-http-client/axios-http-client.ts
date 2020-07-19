5aeced1fb608340f7afe3de1f672b9a3297712f8
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2020-07-19 17:55:47 -0300

    feat: ensure AxiosHttpClient calls axios with correct URL

---
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable class-methods-use-this */
+
+import axios from 'axios'
+import { HttpPostParams } from '../../../data/protocols/http'
+
+export class AxiosHttpClient {
+  async post(params: HttpPostParams<any>): Promise<void> {
+    await axios(params.url)
+  }
+}
