9ec65e099e7d7af98bcab51fc19481383748d2d4
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2020-07-19 14:45:26 -0300

    refactor: move HttpPostClientSpy to its own file

--- src/data/usecases/subscribe/remote-subscribe.spec.ts
+++ src/data/usecases/subscribe/remote-subscribe.spec.ts
@@ -1,16 +1,8 @@
 import { RemoteSubscribe } from './remote-subscribe'
-import { HttpPostClient } from './protocols/http/http-post-client'
+import { HttpPostClientSpy } from '../../test/mock-http-client'
 
 describe('RemoteSubscribe', () => {
   test('Should call HttpPost client with correct URL', async () => {
-    class HttpPostClientSpy implements HttpPostClient {
-      url?: string
-
-      async post(url: string): Promise<void> {
-        this.url = url
-        return Promise.resolve()
-      }
-    }
     const url = 'any_url'
     const httpPostClientSpy = new HttpPostClientSpy()
     const sut = new RemoteSubscribe(url, httpPostClientSpy)
