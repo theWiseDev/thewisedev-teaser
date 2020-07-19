4cf733b458073ecc284fc5fca08d6ebe4b59ec68
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2020-07-19 14:41:23 -0300

    feat: ensure RemoteSubscribe calls HttpPostClient w/ correct URL

---
+++ src/data/usecases/subscribe/remote-subscribe.ts
@@ -0,0 +1,14 @@
+import { HttpPostClient } from './protocols/http/http-post-client'
+
+/* eslint-disable prettier/prettier */
+export class RemoteSubscribe {
+  // eslint-disable-next-line no-useless-constructor
+  constructor(
+    private readonly url: string,
+    private readonly httpPostClient: HttpPostClient,
+  ) { }
+
+  async subscribe(): Promise<void> {
+    await this.httpPostClient.post(this.url)
+  }
+}
