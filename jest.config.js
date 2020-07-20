d1c2fd417530c9db3a4d0fa455cd6d1b54d0d756
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2020-07-20 07:34:57 -0300

    chore: config sass

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,16 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
-    '<rootrDir>/src/**/*.{ts,tsx}'
+    '<rootrDir>/src/**/*.{ts,tsx}',
+    '**/*.d.ts'
   ],
   coverageDirectory: 'coverage',
   testEnvironment: 'jsdom',
   transform: {
     '.+\\.(ts|tsx)$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1',
+    '\\.scss$': 'identity-obj-proxy'
   }
 }
