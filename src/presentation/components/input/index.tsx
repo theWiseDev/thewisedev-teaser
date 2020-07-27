b29ceae0754a7a9d768ebc22127c67c5a37b1120
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2020-07-27 17:00:42 -0300

    chore: disable form button on initial state

--- src/presentation/components/input/index.tsx
+++ src/presentation/components/input/index.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Styles from './styles.scss'
+import Context from '@/presentation/contexts/form/form-context'
 
 type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 
 const Input: React.FC<Props> = (props: Props) => {
+  const { errorMessage } = useContext(Context)
   return (
     <div className={Styles.inputWrapper}>
       <input {...props} />
--- src/presentation/pages/subscribe/index.tsx
+++ src/presentation/pages/subscribe/index.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Styles from './subscribe-styles.scss'
 import logo from '@/presentation/assets/images/logo.svg'
 import wise from '@/presentation/assets/images/illustration.svg'
 import { Input, Button } from '@/presentation/components'
+import Context from '@/presentation/contexts/form/form-context'
 
-const Login: React.FC = () => {
+type StateProps = {
+  isLoading: boolean
+  errorMessage: string
+}
+
+const Subscribe: React.FC = () => {
+  const [state] = useState<StateProps>({
+    isLoading: false,
+    errorMessage: ''
+  })
+
   return (
     <div className={Styles.main}>
       <header className={Styles.header}>
@@ -19,14 +30,16 @@ const Login: React.FC = () => {
             <div className={Styles.contentText}>
               <p>Em breve uma nova plataforma para ampliar seus conhecimentos em desenvolvimento de software e web.</p>
             </div>
-            <form className={Styles.form} action="">
-              <h4 className={Styles.title}>Inscreva-se e seja um alpha tester!</h4>
-              <div className={Styles.fields}>
-                <Input type="text" className={Styles.input} name="userName" placeholder="Nome" />
-                <Input type="email" className={Styles.input} name="userEmail" placeholder="Email" />
-              </div>
-              <Button className={Styles.submit} type="submit">Cadastrar</Button>
-            </form>
+            <Context.Provider value={state}>
+              <form className={Styles.form} action="">
+                <h4 className={Styles.title}>Inscreva-se e seja um alpha tester!</h4>
+                <div className={Styles.fields}>
+                  <Input type="text" className={Styles.input} name="userName" placeholder="Nome" />
+                  <Input type="email" className={Styles.input} name="userEmail" placeholder="Email" />
+                </div>
+                <Button disabled className={Styles.submit} type="submit">Cadastrar</Button>
+              </form>
+            </Context.Provider>
           </div>
           <figure className={Styles.illustration}>
             <img src={wise} alt="the Wise in the beggining of a journey" />
@@ -43,4 +56,4 @@ const Login: React.FC = () => {
   );
 }
 
-export default Login
+export default Subscribe
