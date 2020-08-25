481958cd9e40ed1c9715dacda66045c96682e743
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2020-08-25 18:35:10 -0300

    fix: ensure EmailValidation allows empty value

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -18,4 +18,9 @@ describe('EmailValidation', () => {
     const error = sut.validate(faker.internet.email())
     expect(error).toBeFalsy()
   })
+  test('Should return falsy if email is empty', () => {
+    const sut = makeSut()
+    const error = sut.validate('')
+    expect(error).toBeFalsy()
+  })
 })
--- src/validation/validators/email/email-validation.ts
+++ src/validation/validators/email/email-validation.ts
@@ -5,6 +5,6 @@ export class EmailValidation implements FieldValidation {
   constructor(readonly field: string) { }
   validate(value: string): Error {
     const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    return emailRegex.test(value) ? null : new InvalidFieldError(this.field)
+    return (!value || emailRegex.test(value)) ? null : new InvalidFieldError(this.field)
   }
 }
