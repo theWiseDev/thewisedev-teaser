0fcbf4972e7aaa1fdae60de9d430c4085af78c0a
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2020-08-25 18:27:45 -0300

    test: ensure MinLengthValidation returns falsy if value is valid

--- src/validation/validators/min-length/min-length-validation.spec.ts
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -8,4 +8,10 @@ describe('MinLengthValidation', () => {
     const error = sut.validate('123')
     expect(error).toEqual(new InvalidFieldError('field'))
   })
+
+  test('Should return falsy if value is valid', () => {
+    const sut = new MinLengthValidation('field', 5)
+    const error = sut.validate('12345')
+    expect(error).toBeFalsy()
+  })
 })
