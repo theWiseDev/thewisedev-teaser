4c0774bb5c10e4619b9645e9f36b161eec33b0d6
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2020-08-25 18:49:24 -0300

    test: ensure ValidationComposite returns the first error found

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -5,9 +5,10 @@ describe('ValidationComposite', () => {
   test('Should return error if any validation fails', () => {
     const fieldValidationSpy = new FieldValidationSpy('any_field')
     const fieldValidationSpy2 = new FieldValidationSpy('any_field')
-    fieldValidationSpy2.error = new Error('any_error_message')
+    fieldValidationSpy.error = new Error('first_error_message')
+    fieldValidationSpy2.error = new Error('second_error_message')
     const sut = new ValidationComposite([fieldValidationSpy, fieldValidationSpy2])
     const error = sut.validate('any_field', 'any_value')
-    expect(error).toBe('any_error_message')
+    expect(error).toBe('first_error_message')
   })
 })
