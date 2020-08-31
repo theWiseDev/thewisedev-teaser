aff10dcf17d7225eefbbaca9226fe40f2f2ddced
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2020-08-31 14:54:23 -0300

    feat: add subscribe method to form submit

--- src/presentation/pages/subscribe/index.tsx
+++ src/presentation/pages/subscribe/index.tsx
@@ -1,31 +1,39 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 import logo from '@/presentation/assets/images/logo.svg'
 import wise from '@/presentation/assets/images/illustration.svg'
 import { Input, Button } from '@/presentation/components'
 import Context from '@/presentation/contexts/form/form-context'
 import Validation from '@/presentation/protocols/validation'
+import { Subscribe } from '@/domain/usecases'
 
 type Props = {
   validation: Validation
+  subscribe: Subscribe
 }
 
-const Subscribe: React.FC<Props> = ({ validation }: Props) => {
+const Subscribe: React.FC<Props> = ({ validation, subscribe }: Props) => {
   const [state, setState] = useState({
     loading: false,
     userEmail: '',
     userEmailError: '',
     userName: '',
     userNameError: '',
-    mainError: ''
+    mainError: '',
   })
 
   useEffect(() => {
     setState({
       ...state,
       userEmailError: validation.validate('userEmail', state.userEmail),
-      userNameError: validation.validate('userName', state.userName)
+      userNameError: validation.validate('userName', state.userName),
     })
   }, [state.userName, state.userEmail])
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>): Promise<void> => {
+    event.preventDefault()
+    setState({ ...state, loading: true })
+    await subscribe.subscribe({ name: state.userName, email: state.userEmail })
+  }
 
   return (
     <div className="main">
@@ -37,18 +45,40 @@ const Subscribe: React.FC<Props> = ({ validation }: Props) => {
       <div className="content">
         <div className="container">
           <div className="contentRight">
-            <h2 className="contentTitle">Uma nova abordagem sobre desenvolvimento</h2>
+            <h2 className="contentTitle">
+              Uma nova abordagem sobre desenvolvimento
+            </h2>
             <div className="contentText">
-              <p>Em breve uma nova plataforma para ampliar seus conhecimentos em desenvolvimento de software e web.</p>
+              <p>
+                Em breve uma nova plataforma para ampliar seus conhecimentos em
+                desenvolvimento de software e web.
+              </p>
             </div>
             <Context.Provider value={{ state, setState }}>
-              <form className="form" action="">
+              <form className="form" onSubmit={handleSubmit}>
                 <h4 className="title">Inscreva-se e seja um alpha tester!</h4>
                 <div className="fields">
-                  <Input type="text" className="input" name="userName" value={state.userName} placeholder="Nome" />
-                  <Input type="email" className="input" name="userEmail" value={state.userEmail} placeholder="Email" />
+                  <Input
+                    type="text"
+                    className="input"
+                    name="userName"
+                    value={state.userName}
+                    placeholder="Nome"
+                  />
+                  <Input
+                    type="email"
+                    className="input"
+                    name="userEmail"
+                    value={state.userEmail}
+                    placeholder="Email"
+                  />
                 </div>
-                <Button disabled={!!state.userEmailError || !!state.userNameError} className="submit" type="submit">Cadastrar</Button>
+                <Button
+                  disabled={!!state.userEmailError || !!state.userNameError}
+                  type="submit"
+                >
+                  Cadastrar
+                </Button>
               </form>
             </Context.Provider>
           </div>
@@ -63,8 +93,8 @@ const Subscribe: React.FC<Props> = ({ validation }: Props) => {
           <span className="signature">{`</:{>`}</span>
         </div>
       </footer>
-    </div >
-  );
+    </div>
+  )
 }
 
 export default Subscribe
