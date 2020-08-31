1e5b314cb82eceea05bc32323763d8677a31d883
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2020-08-31 14:49:35 -0300

    feat: add loading class to button when form is submitted

--- src/presentation/components/button/index.tsx
+++ src/presentation/components/button/index.tsx
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import Context from '@/presentation/contexts/form/form-context'
 
-interface Props extends React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
-  children: string;
+interface Props
+  extends React.DetailedHTMLProps<
+    React.ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  > {
+  children: string
 }
 
 const Button: React.FC<Props> = ({ children, ...rest }) => {
+  const { state, setState } = useContext(Context)
   return (
-
-    <button {...rest} className="button">{children}</button>
-  );
+    <button {...rest} className={`button ${state.loading && 'loading'}`}>
+      {children}
+    </button>
+  )
 }
 
 export default Button
