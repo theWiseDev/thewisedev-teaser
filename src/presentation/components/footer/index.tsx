fb8aa75bf42fbb1d4d19cb81ca76f2c9743ebc70
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2022-05-11 18:09:43 -0300

    feat: add dynamic year

--- src/presentation/components/footer/index.tsx
+++ src/presentation/components/footer/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 const Footer: React.FC  = () => {
+  const date = new Date()
   return(
     <footer>
       <h2 className="footer-logo">theWiseDev</h2>
       <h3 className="footer-tagline">Code Wisely!</h3>
-      <p className="footer-copyright">Copyright &copy; 2020 theWiseDev</p>
+      <p className="footer-copyright">Copyright &copy; {date.getFullYear()} theWiseDev</p>
     </footer>
   );
 }
