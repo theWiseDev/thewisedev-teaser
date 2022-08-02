40c5ecc6345d107a2075c7a3583e4f1afd0a66dd
Otavio Lemos <otaviolemos@gmail.com>
2022-08-02 16:48:16 -0300

    refactor: improve logos and text

--- src/presentation/pages/subscribe/WorkshopContact.tsx
+++ src/presentation/pages/subscribe/WorkshopContact.tsx
@@ -14,10 +14,10 @@ const WorkshopContact: React.FC = () => {
         <li className="workshopContact-list">
           <div className="workshopContact-memberBio">
             <p>
-              O professor Otávio Lemos, também integrado ao projeto pessoal da plataforma theWiseDev, tem dado inúmeras palestras, workshops e consultorias junto a empresas que prezam por desenvolvimento de software com profissionalismo. Em particular, ele tem falado principalmente sobre Arquitetura de Software (Arquitetura Limpa), Teste de Software e TDD (Test-Driven Development). Se você tem interesse nesse conteúdo, entre em contato por aqui: <a href="mailto:otaviolemos@thewisedev.com.br"><strong>otaviolemos@thewisedev.com.br</strong></a>
+              O professor Otávio Lemos, também integrado ao projeto pessoal da plataforma theWiseDev, tem dado inúmeras palestras, workshops e consultorias junto a empresas que prezam por desenvolvimento de software com profissionalismo. Em particular, ele tem falado principalmente sobre Arquitetura de Software (Arquitetura Limpa), Teste de Software e TDD (Test-Driven Development). Se você tem interesse nesse conteúdo, entre em contato por <a href="mailto:otaviolemos@thewisedev.com.br"><strong>aqui</strong></a>.
             </p>
             <p>
-              Empresas que já se beneficiaram oficialmente do conteúdo do professor Otávio:
+              Algumas empresas que já se beneficiaram oficialmente do conteúdo do professor Otávio:
             </p>
           </div>
           <div className="workshopContact-companies">
@@ -29,9 +29,6 @@ const WorkshopContact: React.FC = () => {
             </figure>
             <figure>
               <img src={RedventuresLogo} alt="Redventures logo"/>
-            </figure>
-            <figure>
-              <img src={RethinkLogo} alt="Rethink logo"/>
             </figure>
             <figure>
               <img src={HelpperLogo} alt="Helpper Logo"/>
