ab2482f95800cb16dc5ad2cd37c80374508a5765
Otavio Lemos <otaviolemos@gmail.com>
2022-08-02 18:18:28 -0300

    feat: update profile

--- src/presentation/pages/subscribe/Team.tsx
+++ src/presentation/pages/subscribe/Team.tsx
@@ -18,10 +18,12 @@ const Team: React.FC = () => {
               </nav>
             </div>
             <div className="team-memberBio">
-              <p>Otávio Lemos é desenvolvedor, pesquisador e professor de computação. Leciona há dez anos na Universidade Federal de São Paulo (UNIFESP), tem mestrado e doutorado pela Universidade de São Paulo (ICMC), e fez pós-doutorado na Universidade da Califórnia em Irvine (UCI). </p>
+              <p>Otávio Lemos é desenvolvedor, pesquisador, professor e autor. Leciona há mais de dez anos na Universidade Federal de São Paulo (UNIFESP), tem mestrado e doutorado pela Universidade de São Paulo (ICMC), e fez pós-doutorado na Universidade da Califórnia em Irvine (UCI). </p>
               <p>Na UCI, o Prof. Otávio trabalhou em diferentes períodos perfazendo aproximadamente dois anos, em colaboração com a renomada pesquisadora Cristina Lopes. Atualmente ele figura como <a href="https://isr.uci.edu/people/researchers" target="_blank" rel="noopener noreferrer">pesquisador associado</a> ao Institute for Software Research.</p>
               <p>Já apresentou seu trabalho de pesquisa no Google, na Carnegie Mellon University (CMU) e na UCI, além de em inúmeros congressos internacionais. </p>
               <p>Em 2019 iniciou seu trabalho na Internet com um canal pessoal no YouTube que cresceu rapidamente. Esse trabalho representa a aproximação do Prof. Otávio com a indústria de desenvolvimento de software, tópico pelo qual é apaixonado e procura se especializar cada vez mais.</p>
+              <p>Em 2021 ganhou o prêmio <a href="https://mvp.microsoft.com/en-us/PublicProfile/5004312?fullName=Otavio%20Lemos" target="_blank">Microsoft MVP</a>, renovado em 2022-2023.</p>
+              <p>Seu livro <a href="https://otaviolemos.com.br" target="_blank">Arquitetura Limpa na Prática</a> vendeu mais de 1000 cópias digitais até julho de 2022.</p>
             </div>
           </div>
         </li>
